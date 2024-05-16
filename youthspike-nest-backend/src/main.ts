0bdde214120a2664ce35ce18aa3f91f3e54954ad
Md Shayon <mdshayon0@gmail.com>
2024-05-17 01:34:56 +0600

    No change

--- youthspike-nest-backend/src/main.ts
+++ youthspike-nest-backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 
+export const NODE_ENV = 'development';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
