5871c8ed310a7d2ae88332e32a8c57714c891772
Md Shayon <mdshayon0@gmail.com>
2024-06-09 02:01:37 +0600

    Incomplete solving issues with url, and redirect

--- youthspike-admin-frontend/src/middleware.ts
+++ youthspike-admin-frontend/src/middleware.ts
@@ -15,17 +15,16 @@ export const config = {
 
 
 function handleUnauthenticated(request: NextRequest, pathname: string) {
-  // @ts-ignore
   const protectedPages = [...new Set([...directorAuthPages, ...captainAuthPages, ...adminPages])];
 
-  // if (protectedPages.some(page => new RegExp(`${page}(\\/?$)`, 'i').test(pathname))) {
-  //   return NextResponse.redirect(new URL('/login', request.url).toString());
-  // }
-
-
-  if (protectedPages.some(page => new RegExp(`(?:^|\\/|\\/)${page}(?:\\/|$)`, 'i').test(pathname))) {
+  if (protectedPages.some(page => new RegExp(`${page}(\\/?$)`, 'i').test(pathname))) {
     return NextResponse.redirect(new URL('/login', request.url).toString());
   }
+
+
+  // if (protectedPages.some(page => new RegExp(`(?:^|\\/|\\/)${page}(?:\\/|$)`, 'i').test(pathname))) {
+  //   return NextResponse.redirect(new URL('/login', request.url).toString());
+  // }
 
   /**
    *    (?:^|\\/|\\/) matches the start of the string or a / before the page.
