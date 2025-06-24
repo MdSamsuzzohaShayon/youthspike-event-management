2a1c111111b54fc7b4bf2040ac70b4a2443640ba
Md Shayon <mdshayon0@gmail.com>
2025-06-24 12:37:51 +0600

    chore(pm2): temporarily disable clustering mode in production
    - Clustering mode turned off to simplify current deployment
    - Will be re-enabled after resolving compatibility/stability issues

--- youthspike-nest-backend/ecosystem.config.js
+++ youthspike-nest-backend/ecosystem.config.js
@@ -4,6 +4,26 @@
 module.exports = {
   apps: [
     {
+      name: 'nest_backend', // Replace with your app name
+      script: 'dist/main.js', // Replace with the path to your compiled main file
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
+
+/*
+module.exports = {
+  apps: [
+    {
       name: 'BackendAPI_4001',
       script: '/home/shayon/youthspike-nest-backend/dist/main.js',
       env: {
@@ -32,3 +52,4 @@ module.exports = {
     },
   ],
 };
+*/
