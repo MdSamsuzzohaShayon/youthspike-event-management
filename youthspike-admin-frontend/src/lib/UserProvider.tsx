f9c50c349addd99a32685f16153495dd58d1fd09
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2026-08-01 08:36:23 +0600

    Temporary fix by setting event id

--- youthspike-admin-frontend/src/lib/UserProvider.tsx
+++ youthspike-admin-frontend/src/lib/UserProvider.tsx
@@ -4,6 +4,8 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useParams, usePathname } from 'next/navigation'
 import { IUser, IUserContext } from '@/types/user';
 import { getCookie } from '@/utils/clientCookie';
+import SessionStorageService from '@/utils/SessionStorageService';
+import { CURRENT_EVENT } from '@/utils/constant';
 
 export const UserContext = createContext<IUserContext>({ token: null, info: null });
 
@@ -19,6 +21,11 @@ function UserProvider({ children }: React.PropsWithChildren) {
     useEffect(() => {
         const findToken = getCookie('token');
         const findUser = getCookie('user');
+
+        const currentEvent = SessionStorageService.getItem(CURRENT_EVENT);
+        if(!currentEvent){
+            SessionStorageService.setItem(CURRENT_EVENT, "6a3c355236b002d89afc4a49"); // temp
+        }
         // console.log({token: findToken, findUser});
         // Can not set info or token if that is already exist
         if (findToken && findToken !== '' && findToken !== null && (!token || token === '')) setToken(findToken);
